f72c015e71b370c26440c076e17c9dc20c5483b5
MdSuhaibSafwan <suhaibsafwan45@gmail.com>
2026-02-15 00:07:53 +0300

    update vercel settings

--- build_files.sh
+++
@@ -1,7 +0,0 @@
-echo "Now will install packages"
-python3 -m pip install -r requirements.txt
-echo "Package installed"
-
-echo "Collecting static folder"
-python3 manage.py collectstatic --noinput
-echo "Collected"
